67aeb68853db82d84b887076a5a1f0e006d4347c
garyatwalbirkbeck <56833408+garyatwalbirkbeck@users.noreply.github.com>
2024-07-27 06:38:26 +0000

    Completed Hangman thing as well

--- main.py
+++ main.py
@@ -4,13 +4,72 @@ word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+stages = [
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
+
 import random
 
 chosen_word = random.choice(word_list)
 
 len_chosen_word = len(chosen_word)
 no_of_blanks = len_chosen_word
-no_of_turns = len_chosen_word
+no_of_turns = 6
 
 list_dashes_chosen_word = []
 
@@ -21,7 +80,11 @@ print(f"The word you need to guess is : {list_dashes_chosen_word}")
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-while no_of_blanks != 0 and no_of_turns != 0:
+end_of_game = False
+
+while end_of_game != True and no_of_turns != 0:
+    print("You have " + str(no_of_turns) + " turns left")
+    print(stages[6 - no_of_turns])
 
     guess = input(f"Guess a letter: ").lower()
 
@@ -31,14 +94,15 @@ while no_of_blanks != 0 and no_of_turns != 0:
         letter = chosen_word[position]
         if letter == guess:
             list_dashes_chosen_word[position] = letter
-            no_of_blanks = no_of_blanks - 1
             no_of_turns = no_of_turns + 1
 
     print(list_dashes_chosen_word)
     no_of_turns = no_of_turns - 1
-    print("You have " + str(no_of_turns) + " turns left")
 
-if no_of_blanks == 0:
+    if "_" not in list_dashes_chosen_word:
+        end_of_game = True
+
+if end_of_game == True:
     print("You Win")
 elif no_of_turns == 0:
     print("You Lose")
