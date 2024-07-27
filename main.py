18636dec7e832fe3f447c43dd23479eb2f6f9a99
garyatwalbirkbeck <56833408+garyatwalbirkbeck@users.noreply.github.com>
2024-07-27 06:24:43 +0000

    Completed the challenge without the hangman drawing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ chosen_word = random.choice(word_list)
 
 len_chosen_word = len(chosen_word)
 no_of_blanks = len_chosen_word
+no_of_turns = len_chosen_word
 
 list_dashes_chosen_word = []
 
@@ -20,14 +21,24 @@ print(f"The word you need to guess is : {list_dashes_chosen_word}")
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
-guess = input(f"Guess a letter: ").lower()
+while no_of_blanks != 0 and no_of_turns != 0:
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    guess = input(f"Guess a letter: ").lower()
 
-for position in range(len_chosen_word):
-    letter = chosen_word[position]
-    if letter == guess:
-        list_dashes_chosen_word[position] = letter
-        no_of_blanks = no_of_blanks - 1
+    #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-print(list_dashes_chosen_word)
+    for position in range(len_chosen_word):
+        letter = chosen_word[position]
+        if letter == guess:
+            list_dashes_chosen_word[position] = letter
+            no_of_blanks = no_of_blanks - 1
+            no_of_turns = no_of_turns + 1
+
+    print(list_dashes_chosen_word)
+    no_of_turns = no_of_turns - 1
+    print("You have " + str(no_of_turns) + " turns left")
+
+if no_of_blanks == 0:
+    print("You Win")
+elif no_of_turns == 0:
+    print("You Lose")
